71cec26daef031c4229d39594d543fad89152785
thiagofd <thiago.fdornelles@gmail.com>
2020-02-20 00:08:47 -0300

    style: signIn and signUp pages

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,8 +1,48 @@
 import React from 'react';
-import { Text } from 'react-native';
+import PropTypes from 'prop-types';
+import { Image } from 'react-native';
 
-// import { Container } from './styles';
+import logo from '~/assets/logo.png';
 
-export default function SignIn() {
-  return <Text>Ola</Text>;
+import {
+  Container,
+  Form,
+  FormInput,
+  SubmitButton,
+  SignLink,
+  SignLinkText,
+} from './styles';
+
+export default function SignIn({ navigation }) {
+  return (
+    <Container>
+      <Image source={logo} />
+
+      <Form>
+        <FormInput
+          icon="mail-outline"
+          keyboardType="email-address"
+          autoCorrect={false}
+          autoCapitalize="none"
+          placeholder="Digite seu e-mail"
+        />
+
+        <FormInput
+          icon="lock-outline"
+          secureTextEntry
+          placeholder="Sua senha secreta"
+        />
+
+        <SubmitButton onPress={() => {}}>Entrar</SubmitButton>
+      </Form>
+
+      <SignLink onPress={() => navigation.navigate('SignUp')}>
+        <SignLinkText>Criar conta grátis</SignLinkText>
+      </SignLink>
+    </Container>
+  );
 }
+
+SignIn.propTypes = {
+  navigation: PropTypes.oneOfType([PropTypes.any]).isRequired,
+};
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,8 +1,55 @@
 import React from 'react';
-import { View } from 'react-native';
+import PropTypes from 'prop-types';
+import { Image } from 'react-native';
 
-// import { Container } from './styles';
+import logo from '~/assets/logo.png';
 
-export default function SignUp() {
-  return <View />;
+import {
+  Container,
+  Form,
+  FormInput,
+  SubmitButton,
+  SignLink,
+  SignLinkText,
+} from './styles';
+
+export default function SignUp({ navigation }) {
+  return (
+    <Container>
+      <Image source={logo} />
+
+      <Form>
+        <FormInput
+          icon="person-outline"
+          autoCorrect={false}
+          autoCapitalize="words"
+          placeholder="Nome completo"
+        />
+
+        <FormInput
+          icon="mail-outline"
+          keyboardType="email-address"
+          autoCorrect={false}
+          autoCapitalize="none"
+          placeholder="Digite seu e-mail"
+        />
+
+        <FormInput
+          icon="lock-outline"
+          secureTextEntry
+          placeholder="Sua senha secreta"
+        />
+
+        <SubmitButton onPress={() => {}}>Criar conta</SubmitButton>
+      </Form>
+
+      <SignLink onPress={() => navigation.navigate('SignIn')}>
+        <SignLinkText>Já tenho login</SignLinkText>
+      </SignLink>
+    </Container>
+  );
 }
+
+SignUp.propTypes = {
+  navigation: PropTypes.oneOfType([PropTypes.any]).isRequired,
+};
